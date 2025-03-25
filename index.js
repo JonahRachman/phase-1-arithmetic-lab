c214baf56bd29ee29129eb36af27bdc91699392c
JonahRachman <jonahrachman76@gmail.com>
2025-03-25 10:24:47 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const num3 = 24;
+const num4 = 5;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil((Math.random() * 10) + 1);
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 20);
